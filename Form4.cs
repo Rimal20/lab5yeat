b8d4e8610102f02ba874ad581c35028454b8cc6c
Rimal20 <utkadak9@mail.ru>
2023-05-13 18:53:52 +0300

    форма4

--- Form4.cs
+++ Form4.cs
@@ -16,5 +16,12 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            this.Hide();
+            form2.Show();
+        }
     }
 }
