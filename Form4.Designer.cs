ba86ce23169d4c20c03e588745c00eb7549c148c
Rimal20 <utkadak9@mail.ru>
2023-05-13 18:55:57 +0300

    ну же ну

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -46,9 +46,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(69, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(33, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Имя";
             // 
             // label2
             // 
